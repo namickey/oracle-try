1e71014913ae234e62d328434a4b4487dcccd6a2
namickey <teethdai@gmail.com>
2025-04-12 14:32:57 +0900

    add

--- file-export/kokyaku.sql
+++ file-export/kokyaku.sql
@@ -1,10 +1,17 @@
-SET HEADING OFF          -- カラム名を出力しない
-SET FEEDBACK OFF         -- 実行結果の行数を表示しない
-SET COLSEP ','           -- カラムの区切り文字をカンマに設定
-SET LINESIZE 1000        -- 1行の最大文字数を設定
-SET TRIMSPOOL ON         -- 行末の余分なスペースを削除
-SET PAGESIZE 0           -- ページ区切りを無効化
-SET TERMOUT OFF          -- スクリプト実行中の出力を抑制
+-- カラム名を出力しない
+SET HEADING OFF
+-- 実行結果の行数を表示しない
+SET FEEDBACK OFF
+-- カラムの区切り文字をカンマに設定
+SET COLSEP ','
+-- 1行の最大文字数を設定
+SET LINESIZE 1000
+-- 行末の余分なスペースを削除
+SET TRIMSPOOL ON
+-- ページ区切りを無効化
+SET PAGESIZE 0
+-- スクリプト実行中の出力を抑制
+SET TERMOUT OFF
 
 SPOOL kokyaku.csv;
 
