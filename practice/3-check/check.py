7b8e1badf4af7a963b1d8e6a5cc01551b50bcc52
namickey <teethdai@gmail.com>
2025-05-11 07:09:01 +0900

    add

--- practice/3-check/check.py
+++ practice/3-check/check.py
@@ -1,9 +1,11 @@
 
 f = open('../4-import/check-result.txt', 'w')
 for line in open('../2-export/check-req.txt', ):
-    if line.startswith('1') or line.startswith('9'):
+    if not line.startswith('2'):
         continue
-    print(line[14:])
-    if int(line[14:]) < 1000:
-        print('aa')
-
+    print(line[1:4])
+    print(line[14:].strip())
+    if int(line[14:].strip()) < 1000:
+        f.write(line[1:4] + 'OK' + '\n')
+    else:
+        f.write(line[1:4] + 'NG' + '\n')
