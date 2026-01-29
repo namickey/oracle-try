5ae648d167d90ac6f473f5377b74e37bbd927280
namickey <teethdai@gmail.com>
2026-01-29 22:05:30 +0900

    add

--- comp3/tool/split.sh
+++ comp3/tool/split.sh
@@ -10,31 +10,32 @@ OUTPUT_FILE="output_with_newlines.txt"
 # 出力ファイルを初期化
 > "$OUTPUT_FILE"
 
-# hexdumpで全体を16進数に変換し、awkでレコードごとに処理
+# hexdumpで全体を16進数に変換し、awkでレコードごとに処理・バイナリ出力
 hexdump -v -e '1/1 "%02x"' "$INPUT_FILE" | \
-awk '{
+awk -v outfile="$OUTPUT_FILE" '{
     while (length($0) > 0) {
         # 先頭16進文字列として、8文字（4バイト分）を取得
         len_hex = substr($0, 1, 8)
         $0 = substr($0, 9)
         
         # 16進数を10進数に変換
-        cmd = "echo $((16#" len_hex "))"
-        cmd | getline record_length
-        close(cmd)
+        record_length = strtonum("0x" len_hex)
         
         # レコード全体を抽出（長さフィールドを含む）
         record_hex = len_hex substr($0, 1, (record_length - 4) * 2)
         $0 = substr($0, (record_length - 4) * 2 + 1)
         
-        # 16進数をバイナリに戻して出力（改行付き）
-        print record_hex
+        # 16進数をバイナリに変換して出力
+        output = ""
+        for (i = 1; i <= length(record_hex); i += 2) {
+            hex_byte = substr(record_hex, i, 2)
+            decimal = strtonum("0x" hex_byte)
+            output = output sprintf("%c", decimal)
+        }
+        
+        # バイナリデータと改行をファイルに出力
+        printf "%s\n", output >> outfile
     }
-}' | \
-while read hex_line; do
-    # xxdコマンドで16進数をバイナリに変換し、改行を追加
-    echo "$hex_line" | xxd -r -p >> "$OUTPUT_FILE"
-    echo >> "$OUTPUT_FILE"
-done
+}'
 
 echo "変換完了: $OUTPUT_FILE"
